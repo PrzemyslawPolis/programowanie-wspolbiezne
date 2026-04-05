d68bd0a484164e96eb37b42c87b82fb2c03bbc86
PrzemyslawPolis <polis.przem@gmail.com>
2026-04-05 18:17:56 +0200

    Add initial Data layer

---
+++ Data/Vector.cs
@@ -0,0 +1,18 @@
+﻿namespace TP.ConcurrentProgramming.Data
+{
+    internal record Vector : IVector
+    {
+        #region IVector
+
+        public double x { get; init; }
+        public double y { get; init; }
+
+        #endregion IVector
+
+        public Vector(double XComponent, double YComponent)
+        {
+            x = XComponent;
+            y = YComponent;
+        }
+    }
+}
