3d2fb8eb3d34f9e6d71d93ae47f60f7d02925b05
PrzemyslawPolis <polis.przem@gmail.com>
2026-04-09 15:15:26 +0200

    BusinessLogic layer

---
+++ BusinessLogic/BusinessLogicAbstractAPI.cs
@@ -0,0 +1,35 @@
+﻿namespace BusinessLogic
+{
+    public abstract class BusinessLogicAbstractAPI : IDisposable
+    {
+
+        public static BusinessLogicAbstractAPI GetBusinessLogicLayer()
+        {
+            return modelInstance.Value;
+        }
+
+        public static readonly Dimensions GetDimensions = new(10.0, 10.0, 10.0);
+
+        public abstract void Start(int numberOfBalls, Action<IPosition, IBall> upperLayerHandler);
+
+
+        public abstract void Dispose();
+
+
+        private static Lazy<BusinessLogicAbstractAPI> modelInstance = new Lazy<BusinessLogicAbstractAPI>(() => new BusinessLogicImplementation());
+
+    }
+
+    public record Dimensions(double BallDimension, double TableHeight, double TableWidth);
+
+    public interface IPosition
+    {
+        double x { get; init; }
+        double y { get; init; }
+    }
+
+    public interface IBall
+    {
+        event EventHandler<IPosition> NewPositionNotification;
+    }
+}
